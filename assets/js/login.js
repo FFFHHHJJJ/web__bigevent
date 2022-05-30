b32f90ac3e2007870aebe9e677b606f3f71afb8e
fhj96 <1483043597@qq.com>
2022-05-30 16:01:51 +0800

    在login分支上增加了登录和注册功能

---
+++ assets/js/login.js
@@ -0,0 +1,65 @@
+$(function() {
+    // 登录注册切换功能
+    $('#link_reg').click(() => {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#link_login').click(() => {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+// 引入form layer
+    const form = layui.form
+    const layer = layui.layer
+
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+        repwd:(value) => {
+           const pwd = $('#form_reg [name=password]').val() 
+           if(pwd != value) return "两次密码不一致"
+        }
+    })
+
+    // 注册功能
+    const baseURL = 'http://www.liulongbin.top:3007'
+    $('#form_reg').on('submit',(e) => {
+        e.preventDefault()
+        $.ajax({
+            type:'POST',
+            url:'/api/reguser',
+            data:{
+                username:$('#form_reg [name=username]').val(),
+                password:$('#form_reg [name=password]').val(),
+            },
+            success: res => {
+                console.log(res);
+                if(res.status !== 0) return layer.msg(res.message)
+                layer.msg('注册成功！')
+                $('#link_login').click()
+            }
+        })
+    })
+
+// 登录功能
+$('#form_login').on('submit',function(e) {
+    e.preventDefault()
+    $.ajax({
+        type:'POST',
+        url: '/api/login',
+        data: $(this).serialize(),
+        success: res => {
+            // console.log(res);
+            if(res.status !== 0) return layer.msg('登录失败！')
+            layer.msg('登录成功!')
+            // 把token存到本地
+            localStorage.setItem('token',res.token)
+            // 登录成功跳转页面
+            location.href = '/index.html'
+        }
+    })
+})
+
+
+
+})
